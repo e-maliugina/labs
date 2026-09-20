2e1dfb7047c2998e0161a392d9a7d34617dde757
Elizaveta Maliugina <emaliugina@student.cscc.edu>
2026-09-19 21:33:54 -0400

    Added print statements, modified find_upc, created list for find_upc to work, and if-else statement at the end to give an output on the check digit.

--- Labs/Lab8_emaliugina-1.py
+++ Labs/Lab8_emaliugina-1.py
@@ -1,4 +1,29 @@
 upc = str(input("Enter a 12-digit UPC:"))
 
-def find_upc(upc[0:12]):
-    
+print(f"The first 11 digits are '{upc[0:11]}'.")
+print(f"The provided check digit is {upc[11]}.")
+print("Calculating...")
+
+chars = list(upc[0:12])
+
+def find_upc(chars):
+    """ Finds the expected check digit for the entered UPC """
+    add = int(chars[0]) + int(chars[2]) + int(chars[4]) + int(chars[6]) + int(chars[8]) + int(chars[10])
+    product = add * 3
+    result = product + int(chars[1]) + int(chars[3]) + int(chars[5]) + int(chars[7]) + int(chars[9])
+    m = result % 10
+    if m==0:
+        check_digit = 0
+        return check_digit
+    else:
+        check_digit = 10 - m
+        return check_digit
+
+check_digit = find_upc(chars)
+
+print(f"The expected check digit is {check_digit}.")
+
+if int(upc[11]) == check_digit:
+    print("This is a VALID UPC.")
+else:
+    print("This is an INVALID UPC.")
